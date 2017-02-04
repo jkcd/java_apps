e3ef1eaf12389e979b44b34ca866707ca22d6d42
jramamoorthy <jramamoorthy@paypal.com>
2017-02-04 15:23:39 +0530

    view resolver logic

--- SampleSpringMVCApp/src/main/java/jkcd/LoginController.java
+++ SampleSpringMVCApp/src/main/java/jkcd/LoginController.java
@@ -7,9 +7,35 @@ import org.springframework.web.bind.annotation.ResponseBody;
 @Controller
 public class LoginController {
 
-    @RequestMapping(value="login")
+	
+	@RequestMapping(value="login")
+    public String returnLoginPage() {
+        return "login"; 
+    }
+	
+	@RequestMapping(value="ProcessLogin")
+	@ResponseBody
+	public String processLogin(){
+		System.out.println("i got the password");
+		return "this is login handler";
+	}
+	
+	
+	
+	/*
+	 * 
+	 * this below could be used when you dont need 
+	 * view resolver and you are given back 
+	 * the actual content back to dispatcher 
+	 * which sends it back to browser
+	 * because we are using @ResponseBody annotation
+	 * 
+	 */
+	
+    @RequestMapping(value="NoView")
     @ResponseBody
     public String sayHello() {
-        return "Hello World dummy"; 
+        return "This is cool"; 
     }
+    
 }
